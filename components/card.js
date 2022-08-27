acdd1f9bf77badcf6681fa175d6a2f61e4d41cdc
nelo <nelokwaz@gmail.com>
2022-08-27 04:29:28 +0100

    feat: add the basic structure of the card component

--- components/card.js
+++ components/card.js
@@ -1,8 +1,14 @@
-const Card = () => {
+import Image from "next/image";
+import Link from "next/link";
+
+const Card = (props) => {
   return (
-    <div>
-      <p>Card Component</p>
-    </div>
+    <Link href={props.href}>
+      <a>
+        <h2>{props.name}</h2>
+        <Image src={props.imgUrl} width={260} height={160} alt={props.name} />
+      </a>
+    </Link>
   );
 };
 
