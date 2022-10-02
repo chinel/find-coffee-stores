f9160f77b963f8ded0634fe268a3f6944ddb57b1
nelo <nelokwaz@gmail.com>
2022-10-02 12:00:09 +0100

    feat: add rendering to the coffee store detail page

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,13 +1,38 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
+import coffeeStoresData from "../data/coffee-stores.json";
 
 const CoffeeStore = () => {
   const router = useRouter();
   console.log(router);
   return (
     <div>
-      <p>Coffee Store page</p>
+      Coffee Store page {router.query.id}
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+      <Link href="/coffee-store/dynamic">
+        <a>Go to page dynamic</a>
+      </Link>
     </div>
   );
 };
+
+export function getStaticProps(staticProps) {
+  const params = staticProps.params;
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find(
+        (coffeeStore) => coffeeStore.id === params.id
+      ),
+    },
+  };
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+  };
+}
 
 export default CoffeeStore;
