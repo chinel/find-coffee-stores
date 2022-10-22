c0acaf3764be18fda4257c40fa9d949b09f3f09e
nelo <nelokwaz@gmail.com>
2022-10-22 14:47:21 +0100

    fix: add a fallback prop to getStaticPaths

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -2,9 +2,17 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import coffeeStoresData from "../data/coffee-stores.json";
 
-const CoffeeStore = () => {
+const CoffeeStore = (props) => {
   const router = useRouter();
   console.log(router);
+
+  if(router.isFallback){
+    return(
+      <div>
+        Loading...
+        </div>
+    )
+  }
   return (
     <div>
       Coffee Store page {router.query.id}
@@ -14,6 +22,8 @@ const CoffeeStore = () => {
       <Link href="/coffee-store/dynamic">
         <a>Go to page dynamic</a>
       </Link>
+      <p>{props.coffeeStore.name}</p>
+      <p>{props.coffeeStore.address}</p>
     </div>
   );
 };
@@ -23,7 +33,7 @@ export function getStaticProps(staticProps) {
   return {
     props: {
       coffeeStore: coffeeStoresData.find(
-        (coffeeStore) => coffeeStore.id === params.id
+        (coffeeStore) => coffeeStore.id.toString() === params.id
       ),
     },
   };
@@ -32,6 +42,7 @@ export function getStaticProps(staticProps) {
 export function getStaticPaths() {
   return {
     paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+    fallback: true,
   };
 }
 
